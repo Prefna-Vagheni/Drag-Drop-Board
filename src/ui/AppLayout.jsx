3dc3da84f7c52298b32a72c961bfc6617337938a
Prefna Vagheni <prefnavagheni54@gmail.com>
2025-10-03 02:17:06 +0300

    Polish: AppLayout component

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,27 +1,9 @@
-import Header from './Header';
-import Heading from './Heading';
-// import TaskList from './TaskList';
 import DragDropTodoApp from './DragDropTodoApp';
-// import TestDraDroppaleContainer from './TestDraDroppaleContainer';
 
 function AppLayout() {
   return (
     <div className="w-full h-screen text-gray-800 bg-gray-200">
-      {/* <Header /> */}
-      {/* <TestDraDroppaleContainer /> */}
       <DragDropTodoApp />
-
-      {/* <main className="w-full h-screen grid grid-rows-[40px_1fr] grid-cols-3 gap-4 p-3 text-center">
-        <Heading>To Do</Heading>
-        <Heading>In Progress</Heading>
-        <Heading>Completed</Heading>
-
-        <TaskList />
-
-        <h3 className="text-2xl bg-amber-200">Welcome to Task Manager</h3>
-        <Column />
-        <h3 className="text-2xl">Welcome to Task Manager</h3>
-      </main> */}
     </div>
   );
 }
